98c64dcaad4d9df036cf32aceb13d9208849ca1c
Rachael <rachaelewins@hotmail.com>
2020-06-02 11:12:18 +0100

    Clearing out web app attempt in this branch

--- lib/play.rb
+++ lib/play.rb
@@ -1,18 +1,6 @@
-require 'sinatra'
+
 require_relative 'game.rb'
 
-=begin
-class PlaySinatra < Sinatra::Base
-  get '/' do
-    game = Game.new(Interface.new)
-    game.setup
-    game.main_loop
-  end
-end
-=end
-
-#=begin
 game = Game.new(Interface.new)
 game.setup
 game.main_loop
-#=end
